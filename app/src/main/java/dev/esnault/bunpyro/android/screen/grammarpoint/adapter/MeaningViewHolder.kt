7bf0e0f791ee50a741e20bc27cd3ffa1baf04f31
Matthieu Esnault <matthieu.esn0@gmail.com>
2020-03-03 00:05:59 +0900

    Simple displeay of the meaning fields

--- app/src/main/java/dev/esnault/bunpyro/android/screen/grammarpoint/adapter/MeaningViewHolder.kt
+++ app/src/main/java/dev/esnault/bunpyro/android/screen/grammarpoint/adapter/MeaningViewHolder.kt
@@ -2,6 +2,8 @@ package dev.esnault.bunpyro.android.screen.grammarpoint.adapter
 
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
+import dev.esnault.bunpyro.common.hide
+import dev.esnault.bunpyro.common.show
 import dev.esnault.bunpyro.databinding.LayoutGrammarPointMeaningBinding
 import dev.esnault.bunpyro.domain.entities.GrammarPoint
 
@@ -19,6 +21,37 @@ class MeaningViewHolder(
         get() = itemView.context
 
     fun bind(grammarPoint: GrammarPoint?) {
-        // TODO
+        if (grammarPoint == null) {
+            binding.constraintLayout.hide()
+            return
+        }
+
+        binding.constraintLayout.show()
+
+        binding.meaning.text = grammarPoint.meaning
+        
+        val structure = grammarPoint.structure
+        if (!structure.isNullOrBlank()) {
+            binding.structureGroup.show()
+            binding.structureText.text = structure
+        } else {
+            binding.structureGroup.hide()
+        }
+
+        val caution = grammarPoint.caution
+        if (!caution.isNullOrBlank()) {
+            binding.cautionGroup.show()
+            binding.cautionText.text = caution
+        } else {
+            binding.cautionGroup.hide()
+        }
+
+        val nuance = grammarPoint.nuance
+        if (!nuance.isNullOrBlank()) {
+            binding.nuanceGroup.show()
+            binding.nuanceText.text = nuance
+        } else {
+            binding.nuanceGroup.hide()
+        }
     }
 }
