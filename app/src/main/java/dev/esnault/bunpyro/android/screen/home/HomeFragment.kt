73811c08d248c91e609346d69371a68ec3433e5e
Matthieu Esnault <matthieu.esn0@gmail.com>
2020-03-07 01:10:22 +0900

    Remove some UI used to test the ruby span

--- app/src/main/java/dev/esnault/bunpyro/android/screen/home/HomeFragment.kt
+++ app/src/main/java/dev/esnault/bunpyro/android/screen/home/HomeFragment.kt
@@ -2,11 +2,8 @@ package dev.esnault.bunpyro.android.screen.home
 
 
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.view.View
 import dev.esnault.bunpyro.android.screen.base.BaseFragment
-import dev.esnault.bunpyro.android.utils.RubySpan
 import dev.esnault.bunpyro.databinding.FragmentHomeBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -21,17 +18,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
         binding.lessonsButton.setOnClickListener {
             vm.onLessonsTap()
-        }
-
-        setupTextViews()
-    }
-
-    private fun setupTextViews() {
-        binding.furigana.text = SpannableStringBuilder("車が日本\n図書館\n富士山").apply {
-            setSpan(RubySpan("くるま"), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            setSpan(RubySpan("にっぽん"), 2, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            setSpan(RubySpan("としょかん"), 5, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            setSpan(RubySpan("ふじさん"), 9, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
     }
 }
