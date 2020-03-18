80705f806ced7d644924297d10f41ea10dda01a0
Matthieu Esnault <matthieu.esn0@gmail.com>
2020-03-19 02:11:22 +0900

    Add a toàolbar to the settings screen

--- app/src/main/java/dev/esnault/bunpyro/android/screen/settings/SettingsFragment.kt
+++ app/src/main/java/dev/esnault/bunpyro/android/screen/settings/SettingsFragment.kt
@@ -1,59 +1,91 @@
 package dev.esnault.bunpyro.android.screen.settings
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-
 import dev.esnault.bunpyro.R
 import dev.esnault.bunpyro.android.res.toNightMode
 import dev.esnault.bunpyro.android.screen.ScreenConfig
+import dev.esnault.bunpyro.android.screen.base.BaseFragment
+import dev.esnault.bunpyro.android.screen.base.BaseViewModel
 import dev.esnault.bunpyro.common.openUrlInBrowser
+import dev.esnault.bunpyro.databinding.FragmentSettingsBinding
 import dev.esnault.bunpyro.domain.entities.settings.NightModeSetting
 
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.settings_root, rootKey)
+    override val vm: BaseViewModel? = null
+    override val bindingClass = FragmentSettingsBinding::class
 
-        setupDisplay()
-        setupAbout()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+
+        val prefFragment = PreferenceFragment().apply {
+            this.navController = navController
+        }
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, prefFragment)
+            .commit()
     }
 
-    private fun setupDisplay() {
-        findPreference<ListPreference>("night_mode")?.apply {
-            summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
-            setOnPreferenceChangeListener { _, newValue ->
-                val setting = NightModeSetting.fromValue(newValue as? String)
-                val nightMode = setting.toNightMode()
-                AppCompatDelegate.setDefaultNightMode(nightMode)
+    /**
+     * Since we can't add a toolbar to the preference fragment directly,
+     * it's wrapped into another fragment.
+     */
+    class PreferenceFragment : PreferenceFragmentCompat() {
+
+        var navController: NavController? = null
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.settings_root, rootKey)
+
+            setupDisplay()
+            setupAbout()
+        }
+
+        private fun setupDisplay() {
+            findPreference<ListPreference>("night_mode")?.apply {
+                summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
+                setOnPreferenceChangeListener { _, newValue ->
+                    val setting = NightModeSetting.fromValue(newValue as? String)
+                    val nightMode = setting.toNightMode()
+                    AppCompatDelegate.setDefaultNightMode(nightMode)
+                    true
+                }
+            }
+        }
+
+        private fun setupAbout() {
+            findPreference<Preference>("about_app")?.setOnPreferenceClickListener {
+                navigate(SettingsFragmentDirections.actionSettingsToSettingsAbout())
+                true
+            }
+
+            findPreference<Preference>("about_privacy")?.setOnPreferenceClickListener {
+                context?.openUrlInBrowser(ScreenConfig.Url.privacy)
+                true
+            }
+
+            findPreference<Preference>("about_licenses")?.setOnPreferenceClickListener {
+                navigate(SettingsFragmentDirections.actionSettingsToSettingsLicenses())
                 true
             }
         }
-    }
 
-    private fun setupAbout() {
-        findPreference<Preference>("about_app")?.setOnPreferenceClickListener {
-            navigate(SettingsFragmentDirections.actionSettingsToSettingsAbout())
-            true
+        private fun navigate(navDirections: NavDirections) {
+            navController?.navigate(navDirections)
         }
-
-        findPreference<Preference>("about_privacy")?.setOnPreferenceClickListener {
-            context?.openUrlInBrowser(ScreenConfig.Url.privacy)
-            true
-        }
-
-        findPreference<Preference>("about_licenses")?.setOnPreferenceClickListener {
-            navigate(SettingsFragmentDirections.actionSettingsToSettingsLicenses())
-            true
-        }
-    }
-
-    private fun navigate(navDirections: NavDirections) {
-        findNavController().navigate(navDirections)
     }
 }
