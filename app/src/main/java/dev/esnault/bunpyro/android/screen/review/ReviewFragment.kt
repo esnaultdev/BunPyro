87aaa0d289cc0a307098d5e32d37550dce54de10
Matthieu Esnault <matthieu.esn0@gmail.com>
2020-04-24 19:51:56 +0900

    Setup the reviews loading UI

--- app/src/main/java/dev/esnault/bunpyro/android/screen/review/ReviewFragment.kt
+++ app/src/main/java/dev/esnault/bunpyro/android/screen/review/ReviewFragment.kt
@@ -1,7 +1,14 @@
 package dev.esnault.bunpyro.android.screen.review
 
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.observe
+import androidx.navigation.fragment.findNavController
 import dev.esnault.bunpyro.android.screen.base.BaseFragment
+import dev.esnault.bunpyro.android.screen.review.ReviewViewModel.ViewState
+import dev.esnault.bunpyro.android.utils.setupWithNav
 import dev.esnault.bunpyro.databinding.FragmentReviewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -9,4 +16,16 @@ class ReviewFragment : BaseFragment<FragmentReviewBinding>() {
 
     override val bindingClass = FragmentReviewBinding::class
     override val vm: ReviewViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setupWithNav(findNavController())
+
+        vm.viewState.observe(this) { viewState -> bindViewState(viewState) }
+    }
+
+    private fun bindViewState(viewState: ViewState) {
+        binding.loadingGroup.isVisible = viewState is ViewState.Loading
+    }
 }
--- app/src/main/java/dev/esnault/bunpyro/android/screen/review/ReviewViewModel.kt
+++ app/src/main/java/dev/esnault/bunpyro/android/screen/review/ReviewViewModel.kt
@@ -1,9 +1,13 @@
 package dev.esnault.bunpyro.android.screen.review
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.viewModelScope
 import dev.esnault.bunpyro.android.screen.base.BaseViewModel
 import dev.esnault.bunpyro.data.service.review.IReviewService
+import dev.esnault.bunpyro.domain.entities.review.ReviewQuestion
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -11,11 +15,31 @@ class ReviewViewModel(
     private val reviewService: IReviewService
 ) : BaseViewModel() {
 
+    private val _viewState = MutableLiveData<ViewState>(ViewState.Loading)
+    val viewState: LiveData<ViewState>
+        get() = Transformations.distinctUntilChanged(_viewState)
+
+    private var currentState: ViewState? = null
+        set(value) {
+            field = value
+            _viewState.postValue(value)
+        }
+
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            // TODO loading
-            val questions = reviewService.getCurrentReviews()
-            // TODO questions
+            val result = reviewService.getCurrentReviews()
+            currentState = result.fold(
+                onSuccess = { ViewState.Questions(it) },
+                onFailure = { ViewState.Error }
+            )
         }
+    }
+
+    sealed class ViewState {
+        object Loading : ViewState()
+        object Error : ViewState()
+        data class Questions(
+            val questions: List<ReviewQuestion>
+        ) : ViewState()
     }
 }
