5a14330718e182dc7646f8a35ddfc0d0a12cab87
Matthieu Esnault <matthieu.esn0@gmail.com>
2021-08-20 00:37:31 +0900

    Add a new mock question for debug

--- app/src/main/java/dev/esnault/bunpyro/data/network/fake/MockNetworkData.kt
+++ app/src/main/java/dev/esnault/bunpyro/data/network/fake/MockNetworkData.kt
@@ -99,7 +99,43 @@ object Mock {
         ghostReviews = emptyList()
     )
 
-    private fun fakeReview(id: Long) = CurrentReview(
+    // region Current review
+
+    private fun fakeReviewGrammarPoint(id: Long) = Study.GrammarPoint(
+        id = id,
+        title = "偽物 #$id",
+        yomikata = "にせもの",
+        meaning = "Fake",
+        caution = null,
+        structure = null,
+        level = "N5",
+        lesson = 1,
+        nuance = null,
+        incomplete = false,
+        order = 1,
+        sentences = listOf(
+            Study.ExampleSentence(
+                id = id,
+                grammarId = id,
+                japanese = "偽物にご注意。 #$id",
+                english = "Beware of imitations. #$id",
+                nuance = null,
+                order = 1,
+                audioLink = "夏休みでもいいですから、します。.mp3"
+            )
+        ),
+        links = listOf(
+            Study.SupplementalLink(
+                id = id,
+                grammarId = id,
+                site = "Jisho.org",
+                link = "https://jisho.org/word/%E5%81%BD%E7%89%A9",
+                description = "偽物 #$id"
+            )
+        )
+    )
+
+    private fun fakeReview1(id: Long) = CurrentReview(
         id = id,
         nextReview = Date(0L),
         createdAt = Date(0L),
@@ -136,42 +172,50 @@ object Mock {
             sentenceOrder = 0,
             tense = "[present]"
         ),
-        grammarPoint = Study.GrammarPoint(
-            id = id,
-            title = "偽物 #$id",
-            yomikata = "にせもの",
-            meaning = "Fake",
-            caution = null,
-            structure = null,
-            level = "N5",
-            lesson = 1,
-            nuance = null,
-            incomplete = false,
-            order = 1,
-            sentences = listOf(
-                Study.ExampleSentence(
-                    id = id,
-                    grammarId = id,
-                    japanese = "偽物にご注意。 #$id",
-                    english = "Beware of imitations. #$id",
-                    nuance = null,
-                    order = 1,
-                    audioLink = "夏休みでもいいですから、します。.mp3"
-                )
-            ),
-            links = listOf(
-                Study.SupplementalLink(
-                    id = id,
-                    grammarId = id,
-                    site = "Jisho.org",
-                    link = "https://jisho.org/word/%E5%81%BD%E7%89%A9",
-                    description = "偽物 #$id"
-                )
-            )
-        ),
+        grammarPoint = fakeReviewGrammarPoint(id),
         reviewType = ReviewType.NORMAL,
         selfStudy = false
     )
-    val currentReviews = (1L..3L).map { index -> fakeReview(index) }
+    private fun fakeReview2(id: Long) = CurrentReview(
+        id = id,
+        nextReview = Date(0L),
+        createdAt = Date(0L),
+        updatedAt = Date(0L),
+        lastStudiedAt = Date(0L),
+        readings = listOf(1L),
+        history = emptyList(),
+        missedQuestionIds = emptyList(),
+        studiedQuestionIds = listOf(id),
+        complete = true,
+        studyQuestion = Study.Question(
+            id = id,
+            japanese = "<span class='study-area-input'>____</span>と<span class='study-area-input'>____</span>にご注意（ちゅうい）。 #$id",
+            english = "Beware of <strong>imitations</strong> and <strong>imitations</strong>. #$id",
+            alternateAnswers = mapOf(
+                "フェイク" to "Not Gairaigo."
+            ),
+            answer = "にせもの",
+            wrongAnswers = emptyMap(),
+            alternateGrammar = listOf("にせ", "にせにせ"),
+            audioLink = "夏休みでもいいですから、します。.mp3",
+            createdAt = Date(1L),
+            updatedAt = Date(1L),
+            grammarId = id,
+            nuance = "[this is just the vocabulary word for imitations]",
+            sentenceOrder = 0,
+            tense = "[present]"
+        ),
+        grammarPoint = fakeReviewGrammarPoint(id),
+        reviewType = ReviewType.NORMAL,
+        selfStudy = false
+    )
+
+    val currentReviews = listOf(
+        fakeReview1(1L),
+        fakeReview2(2L),
+        fakeReview1(3L)
+    )
     val reviewCount = currentReviews.size
+
+    // endregion
 }
